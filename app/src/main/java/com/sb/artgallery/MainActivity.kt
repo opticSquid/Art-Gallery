7939edc365fe14121c4973a593a750032bc57d65
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2024-06-01 23:27:34 +0530

    Initial Commit

---
+++ app/src/main/java/com/sb/artgallery/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.sb.artgallery
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sb.artgallery.ui.theme.ArtGalleryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ArtGalleryTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxWidth()) {
+                        ImageFrame(name = "Android")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageFrame(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!", modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ArtGalleryTheme {
+        ImageFrame("Android")
+    }
+}
